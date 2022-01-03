b537ac31a2a92a2431b18c084e469a988a5edec3
Willian Rodrigues de Souza <willian.souza@dtidigitial.com.br>
2022-01-03 08:43:03 -0300

    API

---
+++ DevFreela/Core/Enums/ProjectStatusEnum.cs
@@ -0,0 +1,12 @@
+﻿namespace Core.Enums
+{
+    public enum ProjectStatusEnum
+    {
+        Created = 0,
+        InProgress = 1,
+        Suspend = 2,
+        Cancelled = 3,
+        Finished = 4
+    }
+}
+
